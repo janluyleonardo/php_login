0360d9cdf1efc2f2bf9a925513e71e3ed4de7f17
MORJAN-CUN <janluy_moreno@cun.edu.co>
2023-09-08 17:37:31 -0500

    index.php => ajuste para navbar dentro del proyecto login.php => ajuste para navbar dentro del proyecto partials/header.php => ajuste para navbar dentro del proyecto signup.php => ajuste para navbar dentro del proyecto

--- signup.php
+++ signup.php
@@ -1,15 +1,15 @@
 <?php
 
-  require 'database.php';
+require 'database.php';
 
-  $message = '';
+$message = '';
 
-  if (!empty($_POST['email']) && !empty($_POST['password'])) {
-    $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bindParam(':email', $_POST['email']);
-    $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
-    $stmt->bindParam(':password', $password);
+if (!empty($_POST['email']) && !empty($_POST['password'])) {
+  $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+  $stmt = $conn->prepare($sql);
+  $stmt->bindParam(':email', $_POST['email']);
+  $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
+  $stmt->bindParam(':password', $password);
 
   try {
     $stmt->execute();
@@ -17,38 +17,42 @@
   } catch (PDOException $th) {
     $message = 'Sorry there must have been an issue creating your account' . $th;
   }
-    // if ($stmt->execute()) {
-    //   $message = 'Successfully created new user';
-    // } else {
-    //   $message = 'Sorry there must have been an issue creating your account';
-    // }
-  }
+  // if ($stmt->execute()) {
+  //   $message = 'Successfully created new user';
+  // } else {
+  //   $message = 'Sorry there must have been an issue creating your account';
+  // }
+}
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>SignUp</title>
-    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <link rel="stylesheet" href="assets/css/style.css">
-  </head>
-  <body>
 
-    <?php require 'partials/header.php' ?>
+<head>
+  <meta charset="utf-8">
+  <title>SignUp</title>
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+</head>
 
-    <?php if(!empty($message)): ?>
-      <p> <?= $message ?></p>
-    <?php endif; ?>
+<body>
 
-    <h1>SignUp</h1>
-    <span>or <a href="login.php">Login</a></span>
+  <?php require 'partials/header.php' ?>
 
-    <form action="signup.php" method="POST">
-      <input name="email" type="text" placeholder="Enter your email">
-      <input name="password" type="password" placeholder="Enter your Password">
-      <input name="confirm_password" type="password" placeholder="Confirm Password">
-      <input type="submit" value="Submit">
-    </form>
+  <?php if (!empty($message)) : ?>
+    <p> <?= $message ?></p>
+  <?php endif; ?>
 
-  </body>
+  <h1>SignUp</h1>
+  <span>or <a href="login.php">Login</a></span>
+
+  <form action="signup.php" method="POST">
+    <input name="email" type="text" placeholder="Enter your email">
+    <input name="password" type="password" placeholder="Enter your Password">
+    <input name="confirm_password" type="password" placeholder="Confirm Password">
+    <input type="submit" value="Submit">
+  </form>
+
+</body>
+
 </html>
